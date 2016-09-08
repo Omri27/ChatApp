7d5fcf54d957d598f1242f5d032135174ff7cab1
Omri <omri.kodzidlo@gmail.com>
2016-09-08 16:11:22 +0300

    added chats recyclerview

---
+++ app/src/main/java/com/example/omri/chatapp/Chat.java
@@ -0,0 +1,23 @@
+package com.example.omri.chatapp;
+
+/**
+ * Created by Omri on 08/09/2016.
+ */
+public class Chat {
+    private String name;
+
+    public Chat() {
+    }
+
+    public Chat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/omri/chatapp/LobbyActivity.java
+++ app/src/main/java/com/example/omri/chatapp/LobbyActivity.java
@@ -1,20 +1,84 @@
 package com.example.omri.chatapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class LobbyActivity extends AppCompatActivity {
+
+
+
+    public static class ChatViewHolder extends RecyclerView.ViewHolder{
+        public TextView chatName;
+
+        public ChatViewHolder(View itemView) {
+            super(itemView);
+            chatName= (TextView)itemView.findViewById(R.id.chat_name);
+        }
+    }
+    public static final String CHATS= "chats/"+FirebaseAuth.getInstance().getCurrentUser().getUid();
+    private RecyclerView chatRecyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private DatabaseReference ref;
+    private FirebaseRecyclerAdapter<Chat,ChatViewHolder> firebaseRecyclerAdapter;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        chatRecyclerView= (RecyclerView)findViewById(R.id.chat_recycler_view);
+        linearLayoutManager= new LinearLayoutManager(this);
+        ref= FirebaseDatabase.getInstance().getReference();
+        firebaseRecyclerAdapter= new FirebaseRecyclerAdapter<Chat, ChatViewHolder>(
+                Chat.class,
+                R.layout.chat_template,
+                ChatViewHolder.class,
+                ref.child(CHATS )) {
+            @Override
+            protected void populateViewHolder(ChatViewHolder viewHolder, Chat model, int position) {
+                viewHolder.chatName.setText(model.getName());
+
+            }
+        };
+        firebaseRecyclerAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver(){
+            @Override
+            public void onItemRangeInserted(int positionStart,int itemCount){
+                super.onItemRangeInserted(positionStart,itemCount);
+                int chatCount= firebaseRecyclerAdapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+                if(lastVisiblePosition== -1 || (positionStart>=(chatCount-1) && lastVisiblePosition==(positionStart-1))){
+                    chatRecyclerView.scrollToPosition(positionStart);
+                }
+            }
+        });
+        chatRecyclerView.setLayoutManager(linearLayoutManager);
+        chatRecyclerView.setAdapter(firebaseRecyclerAdapter);
+
+
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
