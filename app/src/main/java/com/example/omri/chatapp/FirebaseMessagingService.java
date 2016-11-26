b5c17570d8dae67512d09e63033fc4c86a9a23e9
Omer Shemsheyan <omershemesh95@gmail.com>
2016-11-26 13:04:12 +0200

    fixed notification icon

--- app/src/main/java/com/example/omri/chatapp/FirebaseMessagingService.java
+++ app/src/main/java/com/example/omri/chatapp/FirebaseMessagingService.java
@@ -1,9 +1,11 @@
 package com.example.omri.chatapp;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -39,10 +41,12 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.mipmap.ic_forum_white_24dp)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.mipmap.ic_launcher))
+                .setAutoCancel(true)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setAutoCancel(true)
                 .setVibrate(pattern)
                 .setLights(Color.BLUE,1,1)
                 .setSound(defaultSoundUri)
@@ -51,9 +55,5 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
-
-
-
-
 
 }
